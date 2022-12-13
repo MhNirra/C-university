e78b6108c504f9e06916db03216cfb37c18ab4cf
MhNirra <72897503+MhNirra@users.noreply.github.com>
2022-12-13 02:42:26 -0300

    Update final.c

--- final.c
+++ final.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define T 500//DEFINO LA CANTIDAD DE FILAS DE LA MATRIZ
-#define S 500//DEFINO LA CANTIDAD DE COLUMNAS DE LA MATRIZ 
+#define T 2//DEFINO LA CANTIDAD DE FILAS DE LA MATRIZ
+#define S 2//DEFINO LA CANTIDAD DE COLUMNAS DE LA MATRIZ 
 
 void fillArray(int Array[][S], int inRow, int inColumn){ //FUNCION PARA CREAR LA MATRIZ Y PEDIRLE AL USUARIO QUE INGRESE LOS VALORES DE LA MISMA.
 
@@ -16,17 +16,23 @@ void fillArray(int Array[][S], int inRow, int inColumn){ //FUNCION PARA CREAR LA
 	}
 }
 
+void timeOut(){ //Creo una funcion para poder limpiar la consola y el programa se vea de mejor forma.
+	sleep(1);
+	system("cls");
+}
+
 void printArray(int Array[][S], int inRow, int inColumn){ //FUNCION PARA IMPRIMIR LA MATRIZ DESPUES DE CREARLA.
 
 	int i, j;
 	printf("\nNuestro zona queda de la siguiente manera: \n");
 
-	//CICLO FOR PARA IMPRIMIR LA MATRIZ.
+	//CICLO FOR PARA RECORRER LA COLUMNA Y MOSTRARLE AL USUARIO EL NUMERO DE COLUMNAS DE SU MATRIZ.
 	for(j = 0 ; j < inColumn ; j++){
 		printf("\tCol%i", j);
 
 	}
 	printf("\n");
+	//Ciclo for para recorrer la matriz y mostrarla.
 	for(i = 0 ; i < inRow ; i++){
 		for(j = 0 ; j < inColumn ; j++){
 			printf("\t[%i]  ", Array[i][j]);
@@ -37,16 +43,39 @@ void printArray(int Array[][S], int inRow, int inColumn){ //FUNCION PARA IMPRIMI
 
 }
 
-void volcano(int Array[][S], int inRow, int inColumn){
+void volcano(int Array[][S], int inRow, int inColumn){//Funcion para que el usuario seleccione donde quiere que explote el volcan.
 
 	int row, column, saveValor, i, j;
+
 	printf("\nDonde quiere que explote el volcan: ");
 	printf("\nFila: ");
 	scanf("%i", &row);
 	printf("Columna: ");
 	scanf("%i", &column);
 	printf("El volcan explotara en el punto: [%i][%i], altura %i", row, column, Array[row][column]);
+	printf("\n");
+
+	if(Array[row - 1][column] < Array[row][column]){
+		Array[row - 1][column] = 0;
+	}
+	if(Array[row + 1][column] < Array[row][column]){
+		Array[row + 1][column] = 0;
+	}
+	if(Array[row][column - 1] < Array[row][column]){
+		Array[row][column - 1] =  0;
+	}
+	if(Array[row][column + 1] < Array[row][column]){
+		Array[row][column + 1] = 0;
+	}
+
+	for(i == 0 ; i < inRow ; i++){
+		for(j == 0 ; j < inColumn ; j++){
+			printf("\t[%i]", Array[row][column]);
+		}
+		printf("\n");
+	}
 }
+
 
 int main(){
 
@@ -62,6 +91,7 @@ int main(){
 	scanf("%i", &inColumn);
 
 	fillArray(Array, inRow, inColumn);
+	//timeOut();
 	printArray(Array, inRow, inColumn);
 	volcano(Array, inRow, inColumn);
 
